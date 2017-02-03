e7191114e9da9ae5301bd37a9a2c654c6551d659
App Academy Student <student@appacademy.io>
2017-02-02 16:01:55 -0800

    Implements StackQueue

---
+++ stack.rb
@@ -0,0 +1,25 @@
+class Stack
+  def initialize
+    @store = []
+  end
+
+  def pop
+    @store.pop
+  end
+
+  def push(el)
+    @store.push(el)
+  end
+
+  def peek
+    @store.last
+  end
+
+  def size
+    @store.length
+  end
+
+  def empty?
+    @store.empty?
+  end
+end
